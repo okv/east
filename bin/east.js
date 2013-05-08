f28fcd3d7d47537715c25abf8c2f9a3508229fae
oleg <oleg.poligon@gmail.com>
2013-05-08 14:45:43 +0400

    better commands parsing, almost complete list command

--- bin/east.js
+++ bin/east.js
@@ -5,65 +5,126 @@ var program = require('commander'),
 	fs = require('fs'),
 	DefaultAdapter = require('../lib/adapter');
 
+
 program
-  .version('0.1.0')
-  .usage('[options] <migration1 ...>')
-  .option('-m, --migrate', 'execute migrations')
-  .option('-r, --rollback', 'rollback migrations')
-  .option('-c, --create <name>', 'create migration template')
-  .option('--config <path>', 'config file to use')
-  .option('--adapter <name>', 'which db adapter to use')
-  .option('--dir <dir>', 'dir where migrations stored')
-  .option('-ls, --list', 'list migrations')
-  .parse(process.argv);
+	.version('0.1.0')
+	.option('--config <path>', 'config file to use')
+	.option('--adapter <name>', 'which db adapter to use')
+	.option('--dir <dir>', 'dir where migrations stored');
 
-var cwd = process.cwd();
-var configPath = program.config || Path.join(cwd, '.eastrc');
 
-var dir = Path.join(cwd, 'migrations'),
-	adapter = program.adapter || new DefaultAdapter();
+/**
+ * Create new migration based on template
+ */
+program
+	.command('create <basename>')
+	.description('create migration template')
+	.action(function(basename) {
+		var app = new App(program),
+			path = app.getMigrationPathByName(Date.now() + '_' + basename),
+			templatePath = app.adapter.getTemplatePath();
+		fs.createReadStream(templatePath).pipe(fs.createWriteStream(path));
+		console.log('New migration created: ', path);
+	});
 
-// create new migration base on template
-if (program.create) {
-	var baseName = program.create,
-		path = getMigrationPath(Date.now() + '_' + baseName),
-		templatePath = adapter.getTemplatePath();
-	fs.createReadStream(templatePath).pipe(
-		fs.createWriteStream(path)
-	);
-	console.log('New migration created: ', path);
-} else if (program.list) {
-	console.log('migrations:');
-	getMigrations(function(err, migrations) {
-		if (err) handleError(err);
-		migrations.forEach(function(migration) {
-			console.log('\t', migration);
+
+/**
+ * List migrations
+ */
+program
+	.command('list')
+	.option(
+		'-s, --status <status>', 'which migrations to list (`new`, ' +
+		'`executed` or `all`), `new` by default'
+	)
+	.description('list migrations')
+	.action(function(command) {
+		var app = new App(program),
+			status = command.status || 'new';
+		console.log(status + ' migrations:');
+		app.getMigrationNames(status, function(err, migrations) {
+			if (err) handleError(err);
+			migrations.forEach(function(migration) {
+				console.log('\t', migration);
+			});
 		});
 	});
-} else {
-	program.help();
-}
 
-function getMigrations(callback) {
-	fs.readdir(dir, function(err, paths) {
-		if (err) {
-			callback(err);
-			return;
-		}
-		callback(null, paths.map(getMigrationName));
+
+/**
+ * Main class
+ */
+function App(program) {
+	var cwd = process.cwd();
+	this.configPath = program.config || Path.join(cwd, '.eastrc'),
+	this.migrationsDir = program.dir || Path.join(cwd, 'migrations'),
+	this.adapter = program.adapter || new DefaultAdapter({
+		migrationsDir: this.migrationsDir
 	});
 }
 
-//get migration path by name
-function getMigrationPath(name) {
-	return Path.join(dir, name + '.js');
-}
+App.prototype.getAllMigrationNames = function(callback) {
+	var self = this;
+	fs.readdir(this.migrationsDir, function(err, paths) {
+		if (err) { callback(err); return; }
+		var names = paths
+			.sort()
+			.map(self.getMigrationNameByPath)
+			//skip hidden files
+			.filter(function(name) {
+				return /^\./.test(name) === false;
+			});
+		callback(null, names);
+	});
+};
 
-//get migration name by path
-function getMigrationName(path) {
+App.prototype.getNewMigrationNames = function(callback) {
+	function findNewNames(allNames, executedNames) {
+		var executedNamesHash = {};
+		executedNames.forEach(function(name) { executedNamesHash[name] = 1; });
+		var newNames = allNames.filter(function(name) {
+			return (name in executedNamesHash === false);
+		});
+		callback(null, newNames);
+	}
+	var self = this;
+	self.getAllMigrationNames(function(err, allNames) {
+		if (err) { callback(err); return; }
+		self.adapter.getExecutedMigrationNames(function(err, executedNames) {
+			if (err) { callback(err); return; }
+			findNewNames(allNames, executedNames);
+		});
+	})
+};
+
+App.prototype.getMigrationPathByName = function(name) {
+	return Path.join(this.migrationsDir, name + '.js');
+};
+
+App.prototype.getMigrationNameByPath = function(path) {
 	return Path.basename(path, '.js');
+};
+
+App.prototype.getMigrationNames = function(status, callback) {
+	if (status == 'all') {
+		this.getAllMigrationNames(callback);
+	} else if (status == 'executed') {
+		this.adapter.getExecutedMigrationNames(callback);
+	} else if (status == 'new') {
+		this.getNewMigrationNames(callback);
+	} else {
+		callback(new Error('Unrecognized status `' + status + '`'))
+	}
 }
 
+
+/**
+ * Helpers used above
+ */
 function handleError(err) {
 	throw err;
-}
+};
+
+
+/** Let's start party */
+program.parse(process.argv);
