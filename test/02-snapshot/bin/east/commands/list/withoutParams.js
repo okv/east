fd46bc45757add93cfb9c67da0028c1167aa6295
oleg <oleg.korobenko@gmail.com>
2019-08-30 17:27:07 +0300

    add test for list command

---
+++ test/02-snapshot/bin/east/commands/list/withoutParams.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const tap = require('tap');
+const testUtils = require('../../../../../../testUtils');
+
+tap.mochaGlobals();
+
+const binPath = testUtils.getBinPath('east');
+const describeTitle = 'bin/east list without params';
+
+describe(describeTitle, () => {
+	let commandResult;
+	let migrator;
+
+	before(() => {
+		return Promise.resolve()
+			.then(() => testUtils.createMigrator({init: true}))
+			.then((createdMigrator) => {
+				migrator = createdMigrator;
+
+				return testUtils.createMigrations({
+					migrator,
+					baseNames: ['someMigrationName', 'anotherMigrationName']
+				});
+			});
+	});
+
+	after(() => testUtils.destroyMigrator({migrator}));
+
+	it('should be done without error', () => {
+		const cwd = testUtils.getTestDirPath();
+
+		return Promise.resolve()
+			.then(() => {
+				return testUtils.execAsync(
+					`"${binPath}" list`,
+					{cwd}
+				);
+			})
+			.then((result) => {
+				commandResult = result;
+			});
+	});
+
+	it('stdout should match expected snapshot', () => {
+		tap.matchSnapshot(
+			testUtils.cleanSnapshotData(commandResult.stdout),
+			'output'
+		);
+	});
+});
