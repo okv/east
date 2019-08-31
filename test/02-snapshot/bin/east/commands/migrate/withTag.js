54f858d6b78cad17f6f3b59d71bcd4ca0ac429b8
oleg <oleg.korobenko@gmail.com>
2019-08-31 20:45:46 +0300

    add tests for migrate and rollback with tags

---
+++ test/02-snapshot/bin/east/commands/migrate/withTag.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const tap = require('tap');
+const testUtils = require('../../../../../../testUtils');
+
+tap.mochaGlobals();
+
+const binPath = testUtils.getBinPath('east');
+const describeTitle = 'bin/east migrate without params';
+
+describe(describeTitle, () => {
+	let commandResult;
+	let migrator;
+
+	before(() => {
+		return Promise.resolve()
+			.then(() => testUtils.createMigrator({init: true}))
+			.then((createdMigrator) => {
+				migrator = createdMigrator;
+
+				return testUtils.createMigrations({
+					migrator,
+					baseNames: ['someMigrationName', 'anotherMigrationName']
+				});
+			})
+			.then((names) => {
+				return testUtils.setMigrationTags({
+					migrator,
+					name: names[0],
+					tags: ['someTag']
+				});
+			});
+	});
+
+	after(() => testUtils.destroyMigrator({migrator}));
+
+	it('should be done without error', () => {
+		const cwd = testUtils.getTestDirPath();
+
+		return Promise.resolve()
+			.then(() => {
+				return testUtils.execAsync(
+					`"${binPath}" migrate --tag someTag`,
+					{cwd}
+				);
+			})
+			.then((result) => {
+				commandResult = result;
+			});
+	});
+
+	it('stdout should match expected snapshot', () => {
+		tap.matchSnapshot(
+			testUtils.cleanSnapshotData(commandResult.stdout),
+			'output'
+		);
+	});
+});
