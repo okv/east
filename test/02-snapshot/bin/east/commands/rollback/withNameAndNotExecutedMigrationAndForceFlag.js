f8b9605f10bf140a4b1ff453c88bf9eaae0ed531
oleg <oleg.korobenko@gmail.com>
2019-09-03 17:55:55 +0300

    add tests for migrate and rollback with name and force flag

---
+++ test/02-snapshot/bin/east/commands/rollback/withNameAndNotExecutedMigrationAndForceFlag.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const tap = require('tap');
+const expect = require('expect.js');
+const testUtils = require('../../../../../../testUtils');
+
+tap.mochaGlobals();
+
+const binPath = testUtils.getBinPath('east');
+const describeTitle = (
+	'bin/east rollback with name and not executed migration and force flag'
+);
+
+describe(describeTitle, () => {
+	let commandResult;
+	let migrator;
+
+	before(() => {
+		return Promise.resolve()
+			.then(() => testUtils.createMigrator({init: true}))
+			.then((createdMigrator) => {
+				migrator = createdMigrator;
+
+				return testUtils.createMigrations({
+					migrator,
+					baseNames: ['someMigrationName']
+				});
+			});
+	});
+
+	after(() => testUtils.destroyMigrator({migrator}));
+
+	it('should be done without error', () => {
+		const cwd = testUtils.getTestDirPath();
+
+		return Promise.resolve()
+			.then(() => {
+				return testUtils.execAsync(
+					`"${binPath}" rollback someMigrationName --force`,
+					{cwd}
+				);
+			})
+			.then((result) => {
+				expect(result.stderr).not.ok();
+
+				commandResult = result;
+			});
+	});
+
+	it('stdout should match expected snapshot', () => {
+		tap.matchSnapshot(
+			testUtils.cleanSnapshotData(commandResult.stdout),
+			'output'
+		);
+	});
+});
