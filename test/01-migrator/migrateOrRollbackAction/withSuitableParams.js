7822dbc32f146a47b179fbbd7ce671d8f2f1bf24
oleg <oleg.korobenko@gmail.com>
2019-09-05 18:46:01 +0300

    move migrator filterMigrationNames and migrateOrRollbackAction tests to separate modules

---
+++ test/01-migrator/migrateOrRollbackAction/withSuitableParams.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const tap = require('tap');
+const expect = require('expect.js');
+const testUtils = require('../../../testUtils');
+
+tap.mochaGlobals();
+
+describe('migrator migrate or rollback action with suitable params', () => {
+	let migrator;
+
+	before(() => {
+		return Promise.resolve()
+			.then(() => testUtils.createMigrator({init: true}))
+			.then((createdMigrator) => {
+				migrator = createdMigrator;
+			});
+	});
+
+	after(() => testUtils.destroyMigrator({migrator}));
+
+	const migration = testUtils.makeMigration();
+
+	['migrate', 'rollback'].forEach((action) => {
+		describe(action, () => {
+			it('good migration should be ok', () => migrator[action](migration));
+
+			it('migration which produce eror should pass it', () => {
+				migration[action] = () => {
+					throw new Error(`Test ${action} error`);
+				};
+
+				return Promise.resolve()
+					.then(() => migrator[action](migration))
+					.then((result) => {
+						throw new Error(`Error expected, but got result: ${result}`);
+					})
+					.catch((err) => {
+						expect(err).ok();
+						expect(err).an(Error);
+						expect(err.message).equal(
+							`Error during ${action} "${migration.name
+							}": Test ${action} error`
+						);
+					});
+			});
+		});
+	});
+});
