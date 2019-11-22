e7dc9104f4d5c477f3da8b9361f2822fb52c15e1
oleg <oleg.korobenko@gmail.com>
2019-11-22 19:58:05 +0300

    emit migrate events from manager

--- bin/commands/migrate.js
+++ bin/commands/migrate.js
@@ -14,6 +14,30 @@ exports.Command = Command;
 Command.prototype._execute = function _execute(params) {
 	return Promise.resolve()
 		.then(() => {
+			this.migrationManager.on('beforeMigrateOne', (event) => {
+				this.logger.log(`Migrate "${event.migration.name}"`);
+			});
+
+			this.migrationManager.on('afterMigrateOne', () => {
+				this.logger.log('Migration done');
+			});
+
+			this.migrationManager.on('onSeparatedMigrateNames', (event) => {
+				_(event.executedMigrationNames).each((name) => {
+					this.logger.log(`Skip "${name}" because it's already executed`);
+				});
+			});
+
+			this.migrationManager.on('beforeMigrateMany', (event) => {
+				const names = event.migrationNames;
+
+				if (names.length) {
+					this.logger.log(`Target migrations:\n\t${names.join('\n\t')}`);
+				} else {
+					this.logger.info('Nothing to migrate');
+				}
+			});
+
 			return this.migrationManager.migrate({
 				migrations: _(params.names).isEmpty() ? null : params.names,
 				status: params.command.status,
