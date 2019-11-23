8a8012eeac8394a1de154088138c125a454c5de0
oleg <oleg.korobenko@gmail.com>
2019-11-23 11:10:36 +0300

    emit rollback events from manager

--- bin/commands/rollback.js
+++ bin/commands/rollback.js
@@ -14,6 +14,40 @@ exports.Command = Command;
 Command.prototype._execute = function _execute(params) {
 	return Promise.resolve()
 		.then(() => {
+			this.migrationManager.on('beforeRollbackOne', (event) => {
+				const migration = event.migration;
+
+				if (migration.hasRollback) {
+					this.logger.log(`Rollback "${migration.name}"`);
+				} else {
+					this.logger.log(
+						`Skip "${migration.name}" because rollback function is not set`
+					);
+				}
+			});
+
+			this.migrationManager.on('afterRollbackOne', (event) => {
+				if (event.migration.hasRollback) {
+					this.logger.log('Migration successfully rolled back');
+				}
+			});
+
+			this.migrationManager.on('onSeparatedRollbackNames', (event) => {
+				_(event.newMigrationNames).each((name) => {
+					this.logger.log(`Skip "${name}" because it's not executed yet`);
+				});
+			});
+
+			this.migrationManager.on('beforeRollbackMany', (event) => {
+				const names = event.migrationNames;
+
+				if (names.length) {
+					this.logger.log(`Target migrations:\n\t${names.join('\n\t')}`);
+				} else {
+					this.logger.info('Nothing to rollback');
+				}
+			});
+
 			return this.migrationManager.rollback({
 				migrations: _(params.names).isEmpty() ? null : params.names,
 				status: params.command.status,
