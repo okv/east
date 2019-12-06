f89cb7a178920c58069a896c61797604c35a38d1
oleg <oleg.korobenko@gmail.com>
2019-12-06 21:15:38 +0300

    add test for migrate command with status and particular migrations

---
+++ test/02-snapshot/bin/east/commands/migrate/withStatusAndParticularMigrations.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const tap = require('tap');
+const expect = require('expect.js');
+const testUtils = require('../../../../../../testUtils');
+
+tap.mochaGlobals();
+
+const describeTitle = 'bin/east migrate with status and particular migrations';
+
+describe(describeTitle, () => {
+	let commandErr;
+	let testEnv;
+
+	before(() => {
+		return Promise.resolve()
+			.then(() => testUtils.createEnv({migratorParams: {init: true}}))
+			.then((createdTestEnv) => {
+				testEnv = createdTestEnv;
+
+				return testUtils.createMigrations({
+					migrator: testEnv.migrator,
+					baseNames: ['someMigrationName']
+				});
+			});
+	});
+
+	after(() => testUtils.destroyEnv(testEnv));
+
+	it('should be done without error', () => {
+		return Promise.resolve()
+			.then(() => {
+				const binPath = testUtils.getBinPath('east');
+
+				return testUtils.execAsync(
+					`"${binPath}" migrate --status new someMigrationName`,
+					{cwd: testEnv.dir}
+				);
+			})
+			.then((commandResult) => {
+				throw new Error(
+					`Error expexted but result returned: ${commandResult.stdout}`
+				);
+			})
+			.catch((err) => {
+				expect(err).ok();
+				expect(err.code).equal(1);
+
+				commandErr = err;
+			});
+	});
+
+	it('output should match expected snapshot', () => {
+		const output = `${commandErr.stdout}\n${commandErr.stderr}`;
+
+		tap.matchSnapshot(
+			testUtils.cleanSnapshotData(output),
+			'output'
+		);
+	});
+});
