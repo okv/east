b502c370075215e714c80f7693d0c6d636b53b1b
oleg <oleg.korobenko@gmail.com>
2020-05-01 13:18:32 +0300

    add bin test with unrecognized command

---
+++ test/02-snapshot/bin/east/withUnrecognizedCommand.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const tap = require('tap');
+const expect = require('expect.js');
+const testUtils = require('../../../../testUtils');
+
+tap.mochaGlobals();
+
+describe('bin/east with unrecognized command', () => {
+	let commandErr;
+	let testEnv;
+
+	before(() => {
+		return Promise.resolve()
+			.then(() => testUtils.createEnv({migratorParams: {init: true}}))
+			.then((createdTestEnv) => {
+				testEnv = createdTestEnv;
+			});
+	});
+
+	after(() => testUtils.destroyEnv(testEnv));
+
+	it('should be done with error', () => {
+		return Promise.resolve()
+			.then(() => {
+				const binPath = testUtils.getBinPath('east');
+
+				return testUtils.execAsync(
+					`"${binPath}" notKnownCommand`,
+					{cwd: testEnv.dir}
+				);
+			})
+			.then((commandResult) => {
+				throw new Error(
+					`Error expexted but result returned: ${commandResult.stdout}`
+				);
+			})
+			.catch((err) => {
+				expect(err).ok();
+				expect(err.code).equal(1);
+
+				commandErr = err;
+			});
+	});
+
+	it('output should match expected snapshot', () => {
+		const output = `${commandErr.stdout}\n${commandErr.stderr}`;
+
+		tap.matchSnapshot(
+			testUtils.cleanSnapshotData(output),
+			'output'
+		);
+	});
+});
