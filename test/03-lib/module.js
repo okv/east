bc896ab534a6458d40f63fd7f90c05b430c8c300
oleg <oleg.korobenko@gmail.com>
2020-05-02 16:57:51 +0300

    check types and bin.east package.json values at module test

--- test/03-lib/module.js
+++ test/03-lib/module.js
@@ -14,7 +14,15 @@ describe('module', () => {
 		expect(libModule.MigrationManager).eql(MigrationManager);
 	});
 
-	it('should be set as main in package.json', () => {
+	it('should have lib/index.js as main in package.json', () => {
 		expect(packageJson.main).equal('lib/index.js');
+	});
+
+	it('should have lib/index.d.ts as types in package.json', () => {
+		expect(packageJson.types).equal('lib/index.d.ts');
+	});
+
+	it('should have bin/east.js as bin.east in package.json', () => {
+		expect(packageJson.bin.east).equal('bin/east.js');
 	});
 });
