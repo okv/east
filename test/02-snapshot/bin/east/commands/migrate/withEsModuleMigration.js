e0075fa4fa634b04c37e460b5619c5b698291150
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-05-30 17:05:30 +0300

    add migration test

---
+++ test/02-snapshot/bin/east/commands/migrate/withEsModuleMigration.js
@@ -0,0 +1,59 @@
+const tap = require('tap');
+const expect = require('expect.js');
+const testUtils = require('../../../../../../testUtils');
+
+tap.mochaGlobals();
+
+const describeTitle = 'bin/east migrate with migration as ES Module';
+
+describe(describeTitle, () => {
+	let commandResult;
+	let testEnv;
+
+	before(() => {
+		return Promise.resolve()
+			.then(() => testUtils.createEnv({
+				migratorParams: {
+					init: true,
+					configureParams: {
+						esModules: true,
+						templateText: 'export const migrate = (client, done) => { done(); }'
+					}
+				}
+			}))
+			.then((createdTestEnv) => {
+				testEnv = createdTestEnv;
+
+				return testUtils.createMigrations({
+					migrator: testEnv.migrator,
+					baseNames: ['someMigrationName', 'anotherMigrationName']
+				});
+			});
+	});
+
+	after(() => testUtils.destroyEnv(testEnv));
+
+	it('should be done without error', () => {
+		return Promise.resolve()
+			.then(() => {
+				const binPath = testUtils.getBinPath('east');
+
+				return testUtils.execAsync(
+					`"${binPath}" migrate --es-modules`,
+					{cwd: testEnv.dir}
+				);
+			})
+			.then((result) => {
+				expect(result.stderr).not.ok();
+
+				commandResult = result;
+			});
+	});
+
+	it('stdout should match expected snapshot', () => {
+		tap.matchSnapshot(
+			testUtils.cleanSnapshotData(commandResult.stdout),
+			'output'
+		);
+	});
+});
